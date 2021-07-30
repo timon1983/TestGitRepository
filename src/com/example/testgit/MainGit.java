00c012e746a764651a5a47b1bff0ecbcd7ca05b9
Timon1983 <timon1983@yandex.ru>
2021-07-30 15:03:07 +0300

    +3

--- src/com/example/testgit/MainGit.java
+++ src/com/example/testgit/MainGit.java
@@ -2,6 +2,6 @@ package com.example.testgit;
 
 public class MainGit {
     public static void main(String[] args) {
-        System.out.println("Hi Git!Ha ha!555");
+        System.out.println("Hi Git!Ha ha!555!3333");
     }
 }
