7c2624efd70771cba13f620427809314964508e8
Timon1983 <timon1983@yandex.ru>
2021-07-30 17:54:16 +0300

    add u1

--- src/com/example/testgit/MainGit.java
+++ src/com/example/testgit/MainGit.java
@@ -2,6 +2,6 @@ package com.example.testgit;
 
 public class MainGit {
     public static void main(String[] args) {
-        System.out.println("Hi Git!Ha ha!555!3333!u1");
+        System.out.println("Hi Git!Ha ha!555!3333!u1!u1");
     }
 }
